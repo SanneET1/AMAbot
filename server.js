a4e1f3d1171d42b55be518b350d5d689790d8d09
SanneET1 <eaa26sath@students.eaaa.dk>
2026-09-03 13:52:06 +0200

    post/get methods post på /ask samt opsætning af amabot i express

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (request, response) => {
+  response.render("index", { question: "" });
+});
+
+app.post("/ask", (request, response) => {
+  const question = request.body.question;
+  response.render("index", { question });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
